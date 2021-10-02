a74be652feb78779f6c2ad56f6c1615d629ee020
Garima29 <dadheechgarima371@gmail.com>
2021-10-02 15:28:38 +0530

    adding css, js files, title and description to index page

--- assets/bootstrap/js/index.js
+++
@@ -1,2 +0,0 @@
-const Title ='Portfolio Management';
-const Description='Details of Stock holdings, available Balance, securities, and top recommendations'; 
---
+++ assets/js/index.js
@@ -0,0 +1,7 @@
+import Utility from "../../Utilities/utility.js";
+const Title ='Portfolio Management';
+const Description='Details of Stock holdings, available Balance, securities, and top recommendations'; 
+
+Utility.SetElementValue('lblTitle',Title);
+Utility.SetElementValue('lblTitleDescription',Description);
+
